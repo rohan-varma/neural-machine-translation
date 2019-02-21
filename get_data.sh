fef053e0196eb2984bfe3cd43accad7cd5ae17db
Rohan Varma <rvarm1@gmail.com>
2019-02-21 00:28:34 -0800

    Modified script

--- get_data.sh
+++ get_data.sh
@@ -1,22 +1,29 @@
-if [ -a /data/train/train.de ]
+TRAIN_DATA_PATH='/data/train/'
+TEST_DATA_PATH='/data/test/'
+train_file_de='train.de'
+train_file_en='train.en'
+test_file_de='newstest2012.de'
+test_file_en='newstest2012.en'
+
+if [ -a $TRAIN_DATA_PATH$train_file_de ]
 then
 	echo "Downloading German training file"
 	wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de
 fi
 
-if [ -a /data/train/train.en ]
+if [ -a $TRAIN_DATA_PATH$train_file_en ]
 then
 	echo "Downloading English file..."
 	wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en
 fi
 echo "All training files downloaded in /data/train/"
-if [ -a /data/test/newstest2012.de ]
+if [ -a $TEST_DATA_PATH$test_file_de ]
 then 
 	echo "Downloading German 2012 testing file"
 	wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.de
 fi
 
-if [ -a /data/test/newstest2012.en ]
+if [ -a $TEST_DATA_PATH$test_file_en ]
 then
 	echo "Downloading English 2012 testing file"
 	wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2012.en
